93e7d5c306a9a4ab4c37bc7d00f31a175a82644a
Minsu Kim <syt07100@gmail.com>
2023-08-27 08:03:25 +0900

    fix : prompt 수정

--- src/main/java/com/rimi/backend/global/gpt/service/GetSystemService.java
+++ src/main/java/com/rimi/backend/global/gpt/service/GetSystemService.java
@@ -11,7 +11,7 @@ public class GetSystemService {
             "We're going to give you some questions and user's answers, and you must use them to {}. \n" +
             "You must use korean to answer and also with honorific.\n";
 
-    private final String[] notionQuestions = { "Q1: OOO님의 특징이 잘 드러나도록 한 문장으로 표현해줘. A1: 저는 ",
+    private final String[] notionQuestions = { "Q1: OOO님의 특징이 잘 드러나도록 아주 짧게 한 문장으로 표현해줘. A1: 저는 ",
             "Q2: 업무 스타일에 대해 디테일하게 설명해줘. A2: \n",
             "Q3: 보유한 역량에 대해 디테일하게 설명해줘. A3: \n",
             "Q4: 앞으로의 성장 가능성에 대해 디테일하게 설명해줘. A4: \n",
@@ -23,9 +23,9 @@ public class GetSystemService {
         switch (type) {
             case "getAdvice":
                 return systemContentFormat.format(
-                        "a critic",
-                        "point out how to enrich the answer quality",
-                        "Answer in 300 ~ 400 characters.");
+                        "a critic and also a helper",
+                        "point out to enrich user's answer quality for providing better service",
+                        "Answer in 400 ~ 450 characters.");
             case "findNextQuestion":
                 return systemContentFormat.format(
                         "an interviewer",
@@ -37,8 +37,8 @@ public class GetSystemService {
                                 "3. 당신이 가장 열정적이었던 경험을 말해주세요. \n");
             case "getNotionInput":
                 return systemContentFormat.format(
-                        "men",
-                        "answer appropriately according to the context of the conversation",
+                        "a helper",
+                        "respond according to the context of the conversation",
                         "Based on your understanding of the situation so far, answer following questions.\n" +
                                 "Separate the answers for each question with the $ symbol as a delimiter.\n" +
                                 "Q1: OOO님의 특징이 잘 드러나도록 한 문장으로 표현해줘. A1: 저는 " +
